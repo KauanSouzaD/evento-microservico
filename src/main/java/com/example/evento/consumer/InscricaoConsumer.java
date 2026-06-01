f3ade61158e0101daf47dc22c5d620690c2279c6
Kauan Souza <ks0520512@gmail.com>
2026-05-31 21:46:52 -0300

    alteração

--- src/main/java/com/example/evento/consumer/InscricaoConsumer.java
+++ src/main/java/com/example/evento/consumer/InscricaoConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class InscricaoConsumer {
 
-    @RabbitListener(queues = "email.pedido.queue")
+    @RabbitListener(queues = "email.inscricao.queue")
     public void receberMensagem(String mensagem) {
 
         System.out.println("Mensagem recebida: " + mensagem);
